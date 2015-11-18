7819d5518d425f194d13aa6cbaa9c24b281992ad
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2015-11-18 15:37:35 +0100

    Added: ConfigParser tests.

--- src/test/groovy/com/byoutline/mockserver/internal/ConfigParserSpec.groovy
+++ src/test/groovy/com/byoutline/mockserver/internal/ConfigParserSpec.groovy
@@ -1,13 +1,102 @@
 package com.byoutline.mockserver.internal
 
 import com.byoutline.mockserver.DefaultValues
+import spock.lang.Shared
 import spock.lang.Specification
-
+import spock.lang.Unroll
 
 /**
  * @author Sebastian Kacprzak <sebastian.kacprzak at byoutline.com>
  */
 class ConfigParserSpec extends Specification {
+    @Shared
+    def path = '/a'
+    @Shared
+    def method = 'GET'
+    @Shared
+    def queryKey = 'username'
+    @Shared
+    def queryVal = 'user'
+    @Shared
+    def headerKey = 'sessionId'
+    @Shared
+    def headerVal = '1234'
+    @Shared
+    def bodyContent = "body content"
+    @Shared
+    def simplePath = """{
+            "requests": [
+                {
+                    "method": "$method",
+                    "path": "$path"
+                }
+            ]
+        }"""
+    @Shared
+    def basePath = """{
+            "requests": [
+                {
+                    "method": "$method",
+                    "path": { "base": "$path" }
+                }
+            ]
+        }"""
+    @Shared
+    def patternPath = """{
+            "requests": [
+                {
+                    "method": "$method",
+                    "path": { "urlPattern": "$path" }
+                }
+            ]
+        }"""
+    @Shared
+    def defaultQuery = """{
+            "requests": [
+                {
+                    "method": "$method",
+                    "path": {
+                        "base": "$path",
+                        "queries": { "$queryKey": "$queryVal" }
+                    },
+                }
+            ]
+        }"""
+    @Shared
+    def exactQuery = """{
+            "requests": [
+                {
+                    "method": "$method",
+                    "path": {
+                        "base": "$path",
+                        "queriesMatchingMethod": "EXACT",
+                        "queries": { "$queryKey": "$queryVal" }
+                    },
+                }
+            ]
+        }"""
+    @Shared
+    def header = """{
+            "requests": [
+                {
+                    "method": "$method",
+                    "path": "$path",
+                    "headers": { "$headerKey": "$headerVal"  }
+                }
+            ]
+        }"""
+    @Shared
+    def body = """{
+            "requests": [
+                {
+                    "method": "$method",
+                    "path": "$path",
+                    "bodyPatterns": { "contains": "$bodyContent" }
+                }
+            ]
+        }"""
+
+
     def "should return empty config if config file is empty"() {
         given:
         def reader = new StringConfigReader("")
@@ -29,5 +118,42 @@ class ConfigParserSpec extends Specification {
         def result = instance.parseConfig(json)
         then:
         result.port == 1234
+    }
+
+    @Unroll
+    def "should read request: #expRequest from #config"() {
+        given:
+        def reader = new StringConfigReader(config)
+        when:
+        def json = ConfigParser.getMainConfigJson(reader)
+        def instance = new ConfigParser(reader)
+        def result = instance.parseConfig(json)
+        then:
+        result.responses.size() == 1
+        def requestParams = result.responses.get(0).key
+        requestParams == expRequest
+        where:
+        config       | expRequest
+        simplePath   | createRequest(false, '', [:], [:])
+        basePath     | createRequest(false, '', [:], [:])
+        patternPath  | createRequest(true, '', [:], [:])
+        defaultQuery | createRequest(false, '', [(queryKey): queryVal], DefaultValues.QUERY_MATCHING_METHOD, [:])
+        exactQuery   | createRequest(false, '', [(queryKey): queryVal], MatchingMethod.EXACT, [:])
+        header       | createRequest(false, '', [:], [(headerKey):headerVal])
+        body         | createRequest(false, bodyContent, [:], [:])
+    }
+
+    def createRequest(boolean useRegexForPath,
+                      String bodyMustContain,
+                      Map<String, String> queries,
+                      Map<String, String> headers) {
+        return createRequest(useRegexForPath, bodyMustContain, queries,  DefaultValues.QUERY_MATCHING_METHOD, headers)
+    }
+
+    def createRequest(boolean useRegexForPath,
+                      String bodyMustContain,
+                      Map<String, String> queries, MatchingMethod queriesMatchingMethod,
+                      Map<String, String> headers) {
+        return RequestParams.create(method, path, useRegexForPath, bodyMustContain, queries, queriesMatchingMethod, headers)
     }
 }
