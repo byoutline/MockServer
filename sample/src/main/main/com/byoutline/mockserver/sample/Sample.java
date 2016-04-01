e161990089ed42338443648c27261f394aa84b75
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2016-04-01 12:26:31 +0200

    Added: empty sample module. Refactor: move lib to submodule.

---
+++ sample/src/main/main/com/byoutline/mockserver/sample/Sample.java
@@ -0,0 +1,10 @@
+package com.byoutline.mockserver.sample;
+
+/**
+ * @author Sebastian Kacprzak <sebastian.kacprzak at byoutline.com>
+ */
+public class Sample {
+    public static void main(String... args) {
+
+    }
+}
